b0cd6780f464d84b779fc79cf30ca07494e9de2c
Nagarajan <gnagarajan@deloitte.com>
2021-04-16 01:06:43 +0530

    adding initial netflix build code

---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Banner.css";
+
+function Banner() {
+    return (
+        <header 
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://images.squarespace-cdn.com/content/v1/5825e5b96a496325c2dd987e/1562341935274-1NH6YNYZ85GHRWS8P0H2/ke17ZwdGBToddI8pDm48kDLNi4VuRPPv4o6xvaQaV48UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dkbrwftM_SdKWIRnKq_qAsuqEXeVz9XwiPlkbN4Jo5a7CjLISwBs8eEdxAxTptZAUg/Summer+Movie+Rundown-+Netflix-+banner.png")`,
+                backgroundPosition: "center center",
+            }
+            }
+        >
+
+            
+        </header>
+    )
+}
+
+export default Banner
---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRyxtGA-b8xaQc21EjbnmsFICknQ0M5bk",
+    authDomain: "netflix-build-d0469.firebaseapp.com",
+    projectId: "netflix-build-d0469",
+    storageBucket: "netflix-build-d0469.appspot.com",
+    messagingSenderId: "246317487529",
+    appId: "1:246317487529:web:a3e18bdec1fd24dc7e60b8"
+  };
